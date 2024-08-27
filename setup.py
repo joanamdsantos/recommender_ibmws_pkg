f245912ee8684af2416104beeefd3241cdde4aa3
joanamdsantos <joanamlmsantos@gmail.com>
2024-08-27 14:44:58 +0100

    Update: setup.py and init

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='recommender_ibmws',
+      version='0.1',
+      author= 'Joana Martins dos Santos'
+      author_email= 'joanamlmsantos@gmail.com'
+      description='Recommender System for IBM Watson Studio',
+      packages=setuptools.find_packages(),
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.12',
+    zip_safe=False)
